98000ccfd753aff230e9f81c93abd2406c061f33
Pablomoya24 <pablomoyac24@gmail.com>
2024-05-15 20:55:17 -0600

    Tarea11

---
+++ Ciclos.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+	
+	int i=0;
+	int suma=0;
+	
+	for (i=0;i<=9;i++){
+		suma= suma +5;
+	printf("El numero es %i\n",suma);
+	}
+
+	
+
+
+	
+	
+
+	
+
+    
+
+
+		
+
+   
+   
+return 0;
+}
